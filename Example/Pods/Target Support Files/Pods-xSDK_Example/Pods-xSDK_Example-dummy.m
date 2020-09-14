34d0847c1376dffd9dd067cec08604e670b8f961
kaibuleite <177955297@qq.com>
2020-09-14 10:16:17 +0800

    初始化仓库

---
+++ Example/Pods/Target Support Files/Pods-xSDK_Example/Pods-xSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_xSDK_Example
+@end
---
+++ Example/Pods/Target Support Files/xSDK/xSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_xSDK : NSObject
+@end
+@implementation PodsDummy_xSDK
+@end
