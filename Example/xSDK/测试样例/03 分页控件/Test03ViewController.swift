d28d3b672d53666bbebadd244ac2baa5553134f8
kaibuleite <177955297@qq.com>
2020-09-21 11:37:06 +0800

    Segment组件调整

---
+++ Example/xSDK/测试样例/03 分页控件/Test03ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Test03ViewController.swift
+//  xSDK_Example
+//
+//  Created by Mac on 2020/9/17.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import xSDK
+
+class Test03ViewController: xViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- Example/xSDK/测试视图/11 Segment组件/Kit11ViewController.swift
+++ Example/xSDK/测试视图/11 Segment组件/Kit11ViewController.swift
@@ -7,24 +7,87 @@
 //
 
 import UIKit
+import xSDK
 
-class Kit11ViewController: UIViewController {
-
+class Kit11ViewController: xViewController {
+    
+    @IBOutlet weak var seg1: xSegmentView!
+    @IBOutlet weak var seg2: xSegmentView!
+    @IBOutlet weak var seg3: xSegmentView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .groupTableViewBackground
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func addKit() {
+        self.addSegment1()
+        self.addSegment2()
+        self.addSegment3()
     }
-    */
+    // MARK: - 菜单1——默认
+    public func addSegment1()
+    {
+        let config = xSegmentConfig()
+        config.itemsMargin = 5
+        config.itemChooseTitleColor = .red
+        self.seg1.config = config
+        let arr = ["苹果", "香蕉", "梨子", "芒果", "葡萄"]
+        self.seg1.reload(titleArray: arr, isEqualItemWidth: true, fontSize: 10) {
+            (idx) in
+            x_log(arr[idx])
+        }
+    }
+    
+    // MARK: - 菜单2——超长滚动
+    public func addSegment2()
+    {
+        let config = xSegmentConfig()
+        config.itemsMargin = 10
+        config.itemChooseTitleColor = .blue
+        config.lineColor = .clear
+        config.borderWidth = 1
+        config.itemChooseBorderColor = .blue
+        self.seg2.config = config
+        let arr = ["马铃薯", "菠菜", "西红柿", "茄子", "辣椒",
+                   "黄瓜", "南瓜", "冬瓜", "秋葵", "花菜", "玉米",
+                   "韭菜", "青菜", "萝卜", "芋头", "红薯", "青椒"]
+        self.seg2.reload(titleArray: arr, isEqualItemWidth: false, fontSize: 14) {
+            (idx) in
+            x_log(arr[idx])
+        }
+        self.seg2.choose(idx: 0)
+    }
+    
+    // MARK: - 菜单3——自定义视图（宽度可以自己算）
+    public func addSegment3()
+    {
+        let config = xSegmentConfig()
+        config.itemsMargin = 5
+        config.itemChooseTitleColor = .green
+        config.lineColor = .green
+        self.seg3.config = config
+        let arr = ["🍏", "🍎", "🍐", "🍊", "🍆", "🍳",
+                   "🍋", "🥝", "🥑", "🥔", "🥒", "🥓",
+                   "🍌", "🍒", "🍅", "🌽", "🍞", "🍖",
+                   "🍇", "🍍", "🥐", "🍠", "🍕", "🍟",
+                   "🍉", "🍑", "🌭", "🌶", "🧀", "🥞",
+                   "🍓", "🍈", "🥜", "🥕", "🥖", "🥚", ]
+        var viewArray = [UIView]()
+        arr.forEach {
+            (str) in
+            let lbl = UILabel()
+            lbl.text = str
+            lbl.textAlignment = .center
+            lbl.font = .systemFont(ofSize: 17)
+            // 指定宽度
+            lbl.frame = .init(x: 0, y: 0, width: 30, height: 0)
+            viewArray.append(lbl)
+        }
+        self.seg3.reload(itemViewArray: viewArray) {
+            (idx) in
+            x_log(arr[idx])
+        }
+        self.seg3.choose(idx: 2)
+    }
 
 }
