e3c2f4631242bbc888484b14f0522e97dce9ff08
kaibuleite <177955297@qq.com>
2020-10-17 11:56:45 +0800

    补充文件

--- xSDK/Classes/Kit/05 CollectionViewController/Cells/xCollectionViewCell.swift
+++ xSDK/Classes/Kit/05 CollectionViewController/Cells/xCollectionViewCell.swift
@@ -15,30 +15,30 @@ open class xCollectionViewCell: UICollectionViewCell {
     }
     
     // MARK: - Open Func
-    /// 设置内容占位数据
-    open func setContentPlaceholderData()
-    {
-        /*
-        UIImageView.init().xSetPlaceholderMode()
-        UILabel.init().xSetPlaceholderMode()()
-         */
-    }
     /// 设置内容数据
     open func setContentData(with model : xModel)
     {
-        /*
-        UIImageView.init().xSetNormalMode()()
-        UILabel.init().xSetNormalMode()
-        */
+        /* 设置缓存图片
+        let icon = UIImageView.init()
+        let imgurl = ""
+        if let img = xAppManager.getSDCacheImage(forKey: imgurl) {
+            icon.image = img
+        } else {
+            icon.image = xAppManager.shared.placeholderImage
+        } */
+    }
+    /// 设置滚动结束后中内容数据（一般放下载图片，需要设置tvc的 isOpenReloadDragScrollingEndVisibleCells = true）
+    open func setDragScrollingEndContentData(with model : xModel)
+    {
+        /* 异步下载图片（该方法会在scrollview停止滚动后才调用，优化性能
+        let icon = UIImageView.init()
+        let imgurl = ""
+        icon.sd_setImage(with: imgurl.xToURL(), placeholderImage: xAppManager.shared.placeholderImage, options: .retryFailed, completed: nil)
+         */
     }
     /// 添加按钮事件
-    open func addBtnClickHandler(in xcvc : xCollectionViewController)
+    open func addBtnClickHandler(in xcvc : xCollectionViewController,
+                                 model : xModel)
     {
-        /*
-        guard let cvc = xcvc as? <#xCollectionViewController#> else { return }
-        UIButton.init().xAddClick {
-            (sender) in
-            
-        }*/
     }
 }
