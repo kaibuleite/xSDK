67f7dd85d731be56d432760e34edb2b3e82d8a86
kaibuleite <177955297@qq.com>
2020-10-29 09:27:47 +0800

    更新版本

---
+++ xSDK/Classes/Product_Controller/11 SegmentPage控件/xSegmentPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  xSegmentPageViewController.swift
+//  xSDK
+//
+//  Created by Mac on 2020/10/29.
+//
+
+import UIKit
+
+class xSegmentPageViewController: xViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
